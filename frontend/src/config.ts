d3d408f5966ee443f4ab03213791d8e035635dbd
widusuwuuw <163140675+widusuwuuw@users.noreply.github.com>
2026-01-06 21:08:22 +0800

    优化VS Code内存设置，降低OOM风险

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,8 @@
+// 本地开发用：
 export const API_BASE_URL = 'http://localhost:8000';
+
+// 阿里云服务器：
+// export const API_BASE_URL = 'http://8.148.21.215:8000';
+
 // 如在真机演示，请改成你电脑的局域网IP，例如：
 // export const API_BASE_URL = 'http://192.168.1.100:8000';
